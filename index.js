50bc3d6b52f3e8b13d467cfb0af732034c471ec4
GAO87-ZCB <19035145599@163.com>
2025-04-14 22:15:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { init: initDB, Counter } = require("./db");
+const request = require('request'); // 引入 request 模块
 
 const logger = morgan("tiny");
 
@@ -47,6 +48,33 @@ app.get("/api/wx_openid", async (req, res) => {
   if (req.headers["x-wx-source"]) {
     res.send(req.headers["x-wx-openid"]);
   }
+});
+
+// 获取手机号接口
+app.post('/phone', (req, res) => {
+  // 拼接 Header 中的 x-wx-openid 到接口中
+  const api = `http://api.weixin.qq.com/wxa/getopendata?openid=${req.headers['x-wx-openid']}`;
+
+  request(api, {
+    method: 'POST',
+    body: JSON.stringify({
+      cloudid_list: [req.body.cloudid], // 传入需要换取的 CloudID
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }, (err, resp, body) => {
+    try {
+      const data = JSON.parse(body).data_list[0]; // 从回包中获取手机号信息
+      const phone = JSON.parse(data.json).data.phoneNumber;
+      // 将手机号发送回客户端，此处仅供示例
+      // 实际场景中应对手机号进行打码处理，或仅在后端保存使用
+      res.send(phone);
+    } catch (error) {
+      console.error('获取手机号时发生错误:', error);
+      res.send('get phone failed');
+    }
+  });
 });
 
 const port = process.env.PORT || 80;
