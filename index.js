6d3d9031ec7bfc428569ebdea293f50f926ab741
GAO87-ZCB <19035145599@163.com>
2025-04-15 16:09:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,106 +1,28 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
-const request = require('request'); // 引入 request 模块
-
-const logger = morgan("tiny");
-
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use(logger);
-
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
-
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
-});
-
-// 获取手机号接口
-app.post('/phone', (req, res) => {
-  const openid = req.headers['x-wx-openid'];
-  if (!openid) {
-    console.error('请求头中缺少 x-wx-openid');
-    res.status(400).send({ code: 400, message: '缺少 x-wx-openid' });
-    return;
-  }
-
-  const api = `https://api.weixin.qq.com/wxa/getopendata?openid=${openid}`;
-  console.log('请求微信开放接口的 URL:', api);
-  console.log('请求体:', JSON.stringify({
-    cloudid_list: [req.body.cloudid],
-  }));
-
+// 需要使用 body-parser 处理 JSON 数据
+// 示例路径为 /phone，根据业务场景可自行修改
+app
+.post('/phone', (req, res) => {
+  // 拼接 Header 中的 x-wx-openid 到接口中
+  const api = `http://api.weixin.qq.com/wxa/getopendata?openid=${req.headers['x-wx-openid']}`;
   request(api, {
     method: 'POST',
     body: JSON.stringify({
-      cloudid_list: [req.body.cloudid],
+      cloudid_list: [req.body.cloudid], // 传入需要换取的 CloudID
     }),
     headers: {
       'Content-Type': 'application/json',
     },
   }, (err, resp, body) => {
-    if (err) {
-      console.error('请求微信开放接口时发生错误:', err);
-      res.status(500).send({ code: 500, message: '请求微信开放接口时发生错误' });
-      return;
-    }
-
-    console.log('微信开放接口响应状态码:', resp.statusCode);
-    console.log('微信开放接口响应体:', body);
-
     try {
-      const data = JSON.parse(body).data_list[0];
+      const data = JSON.parse(body).data_list[0]; // 从回包中获取手机号信息
       const phone = JSON.parse(data.json).data.phoneNumber;
-      res.send({ code: 0, data: phone });
+      // 将手机号发送回客户端，此处仅供示例
+      // 实际场景中应对手机号进行打码处理，或仅在后端保存使用
+      res
+.send(phone);
     } catch (error) {
-      console.error('解析微信开放接口响应时发生错误:', error);
-      res.status(500).send({ code: 500, message: '解析微信开放接口响应时发生错误' });
+      res
+.send('get phone failed');
     }
   });
 });
-
-const port = process.env.PORT || 80;
-
-async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
-}
-
-bootstrap();
